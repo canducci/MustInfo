ae04b8e31b8279071b8f7ff6f7258d48b4d788f2
canducci <douglas.canducci@gmail.com>
2012-10-09 19:34:36 -0300

    Added edmx

---
+++ MustInfo/CCI.MustInfo.Data/MISModel.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model '\\mflfcfsnas1b\nbkr4hr\Development\Ideas\MINDS\MustInfoSys\MIS.Data\MISModel.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
